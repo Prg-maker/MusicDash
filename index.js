7205ea0854f2b16a1adf42b0bfd35c3644382736
Prg-maker <71097744+Prg-maker@users.noreply.github.com>
2021-05-21 15:22:29 -0300

    fist commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public/styles"))
+
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname  + "/public/pages/index.html")
+})
+
+app.listen(3000)
+console.log('servidor rodando')
