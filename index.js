c82f8184eef1c66694b81906937985644da0e342
Prg-maker <71097744+Prg-maker@users.noreply.github.com>
2021-05-26 14:56:17 -0300

    create login new

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ const app = express()
 app.use(express.static(__dirname + "/public"))
 app.use(express.static(__dirname + "/public/styles"))
 
-
+const dirname = __dirname
 app.get('/', (req, res)=>{
-    res.sendFile(__dirname  + "/public/pages/index.html")
+    res.sendFile(dirname  + "/public/pages/index.html")
 })
 
 
 app.get('/home', (req, res)=>{
-    res.sendFile(__dirname  + "/public/pages/home.html")
+    res.sendFile(dirname  + "/public/pages/home.html")
 })
 
+app.get('/login', (req , res) =>{
+    res.sendFile(dirname + '/public/pages/login.html')
+})
 
 app.listen(3000)
 console.log('servidor rodando')
